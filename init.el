9c66a9d5cc10164e1dd47b05ab3b12def691e33d
Edwin Dalorzo <edalorzo@backcountry.com>
2018-04-30 22:50:07 -0600

    More configuration settings

--- init.el
+++ init.el
@@ -1,7 +1,12 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;;
+;;    ___ _ __ ___   __ _  ___ ___
+;;   / _ \ '_ ` _ \ / _` |/ __/ __|
+;;  |  __/ | | | | | (_| | (__\__ \
+;; (_)___|_| |_| |_|\__,_|\___|___/
+;;
+
+(setq user-full-name "Edwin Dalorzo")
+(setq user-mail-address "edwin@dalorzo.com")
 
 ;; No splash screen please...jeez!
 (setq inhibit-startup-message t)
